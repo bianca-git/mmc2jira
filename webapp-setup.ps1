9684973d1ec1ff2edd74e35fbc231aa4c220d97e
Bianca W <69695379+bianca-git@users.noreply.github.com>
2023-11-21 20:48:44 +1100

    Update environment variables and deployment configuration

--- webapp-setup.ps1
+++ webapp-setup.ps1
@@ -1,9 +1,9 @@
-SUBSCRIPTION="mmc2jira"
-RESOURCEGROUP="mmc2jira"
-LOCATION="australiasoutheast"
-PLANNAME="mmc2jira"
-PLANSKU="F1"
-SITENAME="mmc2jira"
+# $Env:SUBSCRIPTION="mmc2jira"
+# $Env:RESOURCEGROUP="mmc2jira"
+# $Env:LOCATION="australiasoutheast"
+# $Env:PLANNAME="mmc2jira"
+# $Env:PLANSKU="F1"
+# $Env:SITENAME="mmc2jira"
 
 # login supports device login, username/password, and service principals
 # see https://docs.microsoft.com/en-us/cli/azure/?view=azure-cli-latest#az_login
@@ -11,24 +11,24 @@ az login
 # list all of the available subscriptions
 az account list -o table
 # set the default subscription for subsequent operations
-az account set --subscription $SUBSCRIPTION
+az account set --subscription $Env:SUBSCRIPTION
 # create a resource group for your application
-az group create --name $RESOURCEGROUP --location $LOCATION
+az group create --name $Env:RESOURCEGROUP --location $Env:LOCATION
 # create an appservice plan (a machine) where your site will run
-az appservice plan create --name $PLANNAME --location $LOCATION --sku $PLANSKU --resource-group $RESOURCEGROUP
+az appservice plan create --name $Env:PLANNAME --location $Env:LOCATION --sku $Env:PLANSKU --resource-group $Env:RESOURCEGROUP
 # create the web application on the plan
 # specify the node version your app requires
-az webapp create --name $SITENAME --plan $PLANNAME --resource-group $RESOURCEGROUP
+az webapp create --name $Env:SITENAME --plan $Env:PLANNAME --resource-group $Env:RESOURCEGROUP
 
 # To set up deployment from a local git repository, uncomment the following commands.
 # first, set the username and password (use environment variables!)
-USERNAME=$Env:AZ_USERNAME
-PASSWORD=$Env:AZ_PASSWORD
-az webapp deployment user set --user-name $USERNAME --password $PASSWORD
+# $Env:USERNAME=$Env:AZ_USERNAME
+# $Env:PASSWORD=$Env:AZ_PASSWORD
+az webapp deployment user set --user-name $Env:AZ_USERNAME --password $Env:AZ_PASSWORD
 
 # now, configure the site for deployment. in this case, we will deploy from the local git repository
 # you can also configure your site to be deployed from a remote git repository or set up a CI/CD workflow
-az webapp deployment source config-local-git --name $SITENAME --resource-group $RESOURCEGROUP
+az webapp deployment source config-local-git --name $Env:SITENAME --resource-group $Env:RESOURCEGROUP
 
 # the previous command returned the git remote to deploy to
 # use this to set up a new remote named "azure"
@@ -37,6 +37,6 @@ git remote add azure "https://$USERNAME@$SITENAME.scm.azurewebsites.net/$SITENAM
 git push azure main
 
 # browse to the site
-az webapp browse --name $SITENAME --resource-group $RESOURCEGROUP
+az webapp browse --name $Env:SITENAME --resource-group $Env:RESOURCEGROUP
 
 
